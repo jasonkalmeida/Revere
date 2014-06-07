84d038d95f6dd5fa2cced4bf65ce69d090e861d3
Sergey Serebryakov <sergey@rocketfuel.com>
2014-06-06 20:43:14 -0700

    Better logging for receivemessage

--- receivemessage.js
+++ receivemessage.js
@@ -1,14 +1,21 @@
 exports.receivemessage = function(req, res) {
-	
-	console.log(req);
-	
 
+	 if (req.method == 'POST') {
+		console.log("[200] " + req.method + " to " + req.url);
 
-	res.end("<Response></Response>");
-	
-	
+		req.on('data', function(chunk) {
+			console.log("Received body data:");
+			console.log(chunk.toString());
+		});
 
-	
-	
+		req.on('end', function() {
+			// empty 200 OK response for now
+			res.writeHead(200, "OK", {'Content-Type': 'text/html'});
+			res.end("<Response></Response>");
+		});
+	} else {
+		console.log("[405] " + req.method + " to " + req.url);
+		res.writeHead(405, "Method not supported", {'Content-Type': 'text/html'});
+		res.end('<html><head><title>405 - Method not supported</title></head><body><h1>Method not supported.</h1></body></html>');
+	}
 }
-
--- server.js
+++ server.js
@@ -6,20 +6,20 @@ path = require('path'),//helps with file paths
 fs = require('fs');//helps with file system tasks
 send = require('./sendmessage');
 receive = require('./receivemessage');
- 
+
 //a helper function to handle HTTP requests
 function requestHandler(req, res) {
     var
     content = '',
     fileName = path.basename(req.url),//the file that was requested
     localFolder = __dirname + '/';//where our public files are located
- 
+
     //NOTE: __dirname returns the root folder that
     //this javascript file is in.
- 
-    if(fileName === 'index.html'){//if index.html was requested...
+
+    if(fileName === "/" || fileName === 'index.html'){//if index.html was requested...
         content = localFolder + fileName;//setup the file name to be returned
- 
+
         //reads the file referenced by 'content'
         //and then calls the anonymous function we pass in
         fs.readFile(content,function(err,contents){
@@ -34,7 +34,7 @@ function requestHandler(req, res) {
                 console.dir(err);
             };
         });
-    } 
+    }
 	else if (fileName === 'sendmessage.js'){
 			send.sendmessage();
 	}
@@ -42,7 +42,7 @@ function requestHandler(req, res) {
 	{
 		receive.receivemessage(req, res);
 	}
-	
+
 	else {
         //if the file was not found, set a 404 header...
         res.writeHead(404, {'Content-Type': 'text/html'});
@@ -51,9 +51,9 @@ function requestHandler(req, res) {
         res.end('<h1>Sorry, the page you are looking for cannot be found.</h1>');
     };
 };
- 
+
 //step 2) create the server
 http.createServer(requestHandler)
- 
+
 //step 3) listen for an HTTP request on port 8000
 .listen(8000);
