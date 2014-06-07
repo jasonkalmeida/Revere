d3855a4ac0024ccd7331945494bb44f47b73ac8c
jasonkalmeida <jasonkalmeida>
2014-06-06 21:01:09 -0700

    Parsed HTML

--- receivemessage.js
+++ receivemessage.js
@@ -1,11 +1,17 @@
 exports.receivemessage = function(req, res) {
 
+	var data;
+
 	 if (req.method == 'POST') {
 		console.log("[200] " + req.method + " to " + req.url);
 
 		req.on('data', function(chunk) {
 			console.log("Received body data:");
-			console.log(chunk.toString());
+			data = chunk.toString();
+		    var obj = JSON.parse('{"' + decodeURI(data).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
+			console.log(obj);
+			console.log(obj.Body);
+			console.log(obj.From);
 		});
 
 		req.on('end', function() {
@@ -18,4 +24,10 @@ exports.receivemessage = function(req, res) {
 		res.writeHead(405, "Method not supported", {'Content-Type': 'text/html'});
 		res.end('<html><head><title>405 - Method not supported</title></head><body><h1>Method not supported.</h1></body></html>');
 	}
+	
+
+	
+	
+	
+	
 }
